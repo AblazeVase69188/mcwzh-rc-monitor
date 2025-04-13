21a1974d3d2cb6701100ce7957ba76181cffa69f
AblazeVase69188 <73418189+AblazeVase69188@users.noreply.github.com>
2025-04-13 11:19:58 +0800

    mark Autopatrolled users as green

--- main.py
+++ main.py
@@ -21,12 +21,16 @@ def format_timestamp(timestamp_str): # 将UTC时间改为UTC+8
 def format_comment(comment): # 摘要为空时输出（空）而不是【】
     return f"{Colors.GREEN}（空）{Colors.RESET}" if comment == "" else f"{Colors.GREEN}{comment}{Colors.RESET}"
 
+def format_user(user, special_users): # 有巡查豁免权限的用户标记为绿色
+    return f"{Colors.GREEN}{user}{Colors.RESET}" if user in special_users else f"{Colors.BLUE}{user}{Colors.RESET}"
+
 def print_rc(new_data): # 解析新更改数据并输出
     if new_data:
         for item in new_data:
             length_difference = item['newlen'] - item['oldlen']
             formatted_time = format_timestamp(item['timestamp'])
             comment_display = format_comment(item['comment'])
+            user_display = format_user(item['user'], special_users)
             if item['type'] == 'log': # pycharm自动缩进有点难绷
                 logtype_display = "用户创建" if item['logtype'] == "newusers" else \
                     "删除" if item['logtype'] == "delete" else \
@@ -55,19 +59,19 @@ def print_rc(new_data): # 解析新更改数据并输出
                                                                                 "移动" if item['logaction'] == "move" else \
                                                                                     item['logaction']
                 if item['logtype'] == "upload":
-                    print(f"（{Colors.MAGENTA}上传日志{Colors.RESET}）{Colors.CYAN}{formatted_time}{Colors.RESET}，{Colors.BLUE}{item['user']}{Colors.RESET}对{Colors.BLUE}{item['title']}{Colors.RESET}执行了{Colors.MAGENTA}{logaction_display}{Colors.RESET}操作，摘要为{comment_display}。")
+                    print(f"（{Colors.MAGENTA}上传日志{Colors.RESET}）{Colors.CYAN}{formatted_time}{Colors.RESET}，{Colors.BLUE}{user_display}{Colors.RESET}对{Colors.BLUE}{item['title']}{Colors.RESET}执行了{Colors.MAGENTA}{logaction_display}{Colors.RESET}操作，摘要为{comment_display}。")
                     print(f"（{Colors.YELLOW}https://zh.minecraft.wiki/?diff={item['revid']}{Colors.RESET}）")
                     print(f"（特殊巡查：https://zh.minecraft.wiki/index.php?curid={item['pageid']}&action=markpatrolled&rcid={item['rcid']}）",end='\n\n')
                 elif item['logtype'] == "move":
-                    print(f"（{Colors.MAGENTA}移动日志{Colors.RESET}）{Colors.CYAN}{formatted_time}{Colors.RESET}，{Colors.BLUE}{item['user']}{Colors.RESET}对{Colors.BLUE}{item['title']}{Colors.RESET}执行了{Colors.MAGENTA}{logaction_display}{Colors.RESET}操作，摘要为{comment_display}。")
+                    print(f"（{Colors.MAGENTA}移动日志{Colors.RESET}）{Colors.CYAN}{formatted_time}{Colors.RESET}，{Colors.BLUE}{user_display}{Colors.RESET}对{Colors.BLUE}{item['title']}{Colors.RESET}执行了{Colors.MAGENTA}{logaction_display}{Colors.RESET}操作，摘要为{comment_display}。")
                     print(f"（{Colors.YELLOW}https://zh.minecraft.wiki/?diff={item['revid']}{Colors.RESET}）", end='\n\n')
                 else:
-                    print(f"（{Colors.MAGENTA}{logtype_display}日志{Colors.RESET}）{Colors.CYAN}{formatted_time}{Colors.RESET}，{Colors.BLUE}{item['user']}{Colors.RESET}对{Colors.BLUE}{item['title']}{Colors.RESET}执行了{Colors.MAGENTA}{logaction_display}{Colors.RESET}操作，摘要为{comment_display}。",end='\n\n')
+                    print(f"（{Colors.MAGENTA}{logtype_display}日志{Colors.RESET}）{Colors.CYAN}{formatted_time}{Colors.RESET}，{Colors.BLUE}{user_display}{Colors.RESET}对{Colors.BLUE}{item['title']}{Colors.RESET}执行了{Colors.MAGENTA}{logaction_display}{Colors.RESET}操作，摘要为{comment_display}。",end='\n\n')
             elif item['type'] == 'edit':
-                print(f"{Colors.CYAN}{formatted_time}{Colors.RESET}，{Colors.BLUE}{item['user']}{Colors.RESET}在{Colors.BLUE}{item['title']}{Colors.RESET}做出编辑，字节更改为{Colors.CYAN}{length_difference}{Colors.RESET}，摘要为{comment_display}。")
+                print(f"{Colors.CYAN}{formatted_time}{Colors.RESET}，{Colors.BLUE}{user_display}{Colors.RESET}在{Colors.BLUE}{item['title']}{Colors.RESET}做出编辑，字节更改为{Colors.CYAN}{length_difference}{Colors.RESET}，摘要为{comment_display}。")
                 print(f"（{Colors.YELLOW}https://zh.minecraft.wiki/?diff={item['revid']}{Colors.RESET}）", end='\n\n')
             elif item['type'] == 'new':
-                print(f"{Colors.CYAN}{formatted_time}{Colors.RESET}，{Colors.BLUE}{item['user']}{Colors.RESET}创建{Colors.BLUE}{item['title']}{Colors.RESET}，字节更改为{Colors.CYAN}{length_difference}{Colors.RESET}，摘要为{comment_display}。")
+                print(f"{Colors.CYAN}{formatted_time}{Colors.RESET}，{Colors.BLUE}{user_display}{Colors.RESET}创建{Colors.BLUE}{item['title']}{Colors.RESET}，字节更改为{Colors.CYAN}{length_difference}{Colors.RESET}，摘要为{comment_display}。")
                 print(f"（{Colors.YELLOW}https://zh.minecraft.wiki/?diff={item['revid']}{Colors.RESET}）", end='\n\n')
             elif item['type'] == 'external': # 未知类型，直接输出原文
                 print(item, end='\n\n')
@@ -90,7 +94,14 @@ with open("config.json", "r") as config_file:
 site = wiki.Site("https://zh.minecraft.wiki/api.php", retry_after_conn=30)
 site.login(username, password)
 
-print("启动成功",end='\n\n')
+print("启动成功", end='\n\n')
+
+try:
+    with open('Autopatrolled_user.json', 'r', encoding='utf-8') as special_users_file:
+        special_users = json.load(special_users_file)
+except FileNotFoundError:
+    print("巡查豁免权限用户列表获取失败", end='\n\n')
+    special_users = []
 
 # 最近更改：不要获取机器人编辑，每次获取25个编辑（SimpleBatchUpload大约每秒最多上传5个文件）
 rc_url = "https://zh.minecraft.wiki/api.php?action=query&format=json&list=recentchanges&formatversion=2&rcprop=user%7Ctitle%7Ctimestamp%7Cids%7Cloginfo%7Csizes%7Ccomment&rcshow=!bot&rclimit=25&rctype=edit%7Cnew%7Clog%7Cexternal"
